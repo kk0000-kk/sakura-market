08870eada9e0d066c245e18322946bc6748a4abf
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-04 22:34:06 +0900

    nick_name追加、migrate

--- db/migrate/20231104132906_devise_create_users.rb
+++ db/migrate/20231104132906_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
       t.string   :reset_password_token
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :nick_name, null: false
       t.timestamps null: false
     end
 
