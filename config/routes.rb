23bd99eeed761aa17f42bd294270aedb53397991
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-06 17:09:54 +0900

    ホームページを整備

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static_pages#home'
 end
