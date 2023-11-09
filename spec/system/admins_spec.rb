4e42b866eb79863ed8677ad01497b56336cd3176
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-09 19:09:28 +0900

    レビュー対応: 構造化改善

--- spec/system/admins_spec.rb
+++ spec/system/admins_spec.rb
@@ -1,21 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Admins', type: :system do
-  it '管理者はログインすると、admins_rootにリダイレクトされる' do
-    admin = FactoryBot.create(:admin)
-    visit admins_root_path
-    fill_in 'Email', with: admin.email
-    fill_in 'Password', with: admin.password
-    click_button 'Log in'
-    expect(page).to have_current_path admins_root_path
-  end
+  describe '管理者ログイン' do
+    before do
+      create(:admin, email: 'admin@example.com', password: 'passwordpassword', password_confirmation: 'passwordpassword')
+    end
 
-  it 'ログインに失敗するとログイン画面のまま' do
-    admin = FactoryBot.create(:admin)
-    visit admins_root_path
-    fill_in 'Email', with: admin.email
-    fill_in 'Password', with: 'machigattapassword'
-    click_button 'Log in'
-    expect(page).to have_current_path new_admin_session_path
+    context '認証情報が正しい場合' do
+      it '管理者はログインすると、admins_rootにリダイレクトされる' do
+        visit admins_root_path
+        fill_in 'Email', with: 'admin@example.com'
+        fill_in 'Password', with: 'passwordpassword'
+        click_button 'Log in'
+        expect(page).to have_current_path admins_root_path
+      end
+    end
+
+    context '認証情報が正しくない場合' do
+      it 'ログインに失敗するとログイン画面のまま' do
+        visit admins_root_path
+        fill_in 'Email', with: 'admin@example.com'
+        fill_in 'Password', with: 'machigattapassword'
+        click_button 'Log in'
+        expect(page).to have_current_path new_admin_session_path
+      end
+    end
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,40 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
-  it 'ユーザーはログインすると、/homeにリダイレクトされる' do
-    user = FactoryBot.create(:user)
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-    expect(page).to have_current_path home_path
-  end
+  describe 'ユーザーログイン' do
+    before do
+      create(:user, email: 'user@example.com', password: 'passwordpassword', password_confirmation: 'passwordpassword')
+    end
 
-  it '未ログインでusers/editを開いて、userログインして、users/editにリダイレクトされる' do
-    user = FactoryBot.create(:user)
-    visit edit_user_registration_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-    expect(page).to have_current_path edit_user_registration_path
-  end
+    context '認証情報が正しい場合' do
+      it 'ユーザーはログインすると、/homeにリダイレクトされる' do
+        visit new_user_session_path
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'passwordpassword'
+        click_button 'Log in'
+        expect(page).to have_current_path home_path
+      end
 
-  it 'ログインに失敗するとログイン画面のまま' do
-    user = FactoryBot.create(:user)
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'machigattapassword'
-    click_button 'Log in'
-    expect(page).to have_current_path new_user_session_path
-  end
+      it '未ログインでusers/editを開いて、userログインして、users/editにリダイレクトされる' do
+        visit edit_user_registration_path
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'passwordpassword'
+        click_button 'Log in'
+        expect(page).to have_current_path edit_user_registration_path
+      end
+    end
 
-  it 'ユーザーはログインしてからadmins_rootに行ってもログインを求められる' do
-    user = FactoryBot.create(:user)
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-    visit admins_root_path
-    expect(page).to have_current_path new_admin_session_path
+    context '認証情報が正しくない場合' do
+      it 'ログインに失敗するとログイン画面のまま' do
+        visit new_user_session_path
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'machigattapassword'
+        click_button 'Log in'
+        expect(page).to have_current_path new_user_session_path
+      end
+
+      it 'ユーザーはログインしてからadmins_rootに行ってもログインを求められる' do
+        visit new_user_session_path
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'passwordpassword'
+        click_button 'Log in'
+        visit admins_root_path
+        expect(page).to have_current_path new_admin_session_path
+      end
+    end
   end
 end
