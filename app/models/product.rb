59dc340cd4f4c1fd7ca9ef2fa4b14b6761bf5c6a
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-17 16:48:20 +0900

    画像をリサイズする

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :image do |attachable|
+    attachable.variant :display, resize_to_limit: [500, 500]
+  end
   validates :name, presence: true
   validates :price, presence: true
   validates :position, presence: true
