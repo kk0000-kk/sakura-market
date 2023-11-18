e8b27a786c02b2511234f5bc97ebaf7dbf284af3
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-18 23:21:24 +0900

    レビュー対応: productのアクションを使うもののみに絞る

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     root 'static_pages#home'
     resources :admins, only: %i[index new create destroy]
     resources :users, only: %i[index destroy]
-    resources :products
+    resources :products, only: %i[index new create edit update destroy]
   end
 
   root 'static_pages#index'
