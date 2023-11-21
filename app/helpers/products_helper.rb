72e197e7e62d4b28cb17ceac876bcb6c70eb36f1
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-21 22:15:09 +0900

    画像表示ヘルパーのvariantを引数に追加

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,5 @@
 module ProductsHelper
-  def product_image(product, size)
-    image_tag product.image.attached? ? product.image.variant(:thumbnail) : 'no_image.png', size:
+  def product_image(product, variant, size)
+    image_tag product.image.attached? ? product.image.variant(variant) : 'no_image.png', size:
   end
 end
