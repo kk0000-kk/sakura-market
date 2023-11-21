873847beb1417e2ddd0e27c858b8e4066fa5ecd1
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-21 16:52:44 +0900

    レビュー対応: ログイン状態のテストは重複しているので削除して、検証項目追加

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -2,24 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Products', type: :system do
   describe '商品一覧操作' do
-    let!(:user) { create(:user) }
     let!(:product) { create(:product, name: 'メープルパン', price: 98765, description: '美味しいパンです', disabled: false, position: 1) }
 
-    context 'ユーザーがログインしていない場合' do
-      it '商品を閲覧できる' do
-        visit root_path
-        click_link '詳細', match: :first
-        expect(page).to have_current_path product_path(product)
-      end
+    before do
+      create(:product, name: 'レーズンパン', price: 12345, description: '好き嫌いの激しいパンです', disabled: true, position: 2)
     end
 
-    context 'ユーザーがログインしている場合' do
+    context 'ユーザーがログインしていない場合でも' do
       it '商品を閲覧できる' do
-        login_as(user, scope: :user)
         visit root_path
-        expect(page).to have_current_path root_path
+        expect(page).to have_content 'メープルパン'
+        expect(page).not_to have_content 'レーズンパン'
         click_link '詳細', match: :first
         expect(page).to have_current_path product_path(product)
+        expect(page).to have_content 'メープルパン'
+        expect(page).to have_content '108,641円'  # 税込価格: (98765 * 1.1).floor
+        expect(page).to have_content '98,765円'
+        expect(page).to have_content '美味しいパンです'
       end
     end
   end
