be9bb02a3170b66b5f31bc17a8388fe2ccbe9527
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-22 14:15:47 +0900

    current_cartをアプリケーションコントローラーで定義しておく

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def current_cart
+    if user_signed_in?
+      current_user.cart || current_user.create_cart!
+    end
+  end
 end
