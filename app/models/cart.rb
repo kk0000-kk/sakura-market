dcd500aa67212c752ea0bbc3e1dc7d867c9e34c6
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-23 21:27:26 +0900

    カートに合計値を表示する

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,4 +12,12 @@ class Cart < ApplicationRecord
       cart_items.create!(product_id:, quantity:)
     end
   end
+
+  def total_price
+    (cart_items.map { |item| item.product.price * item.quantity }).sum
+  end
+
+  def total_price_with_tax
+    (total_price * 1.1).floor
+  end
 end
