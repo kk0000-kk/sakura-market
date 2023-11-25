da8388cd752f906718a407112247fd5b521164ee
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-25 22:09:48 +0900

    レビュー対応: current_cartをヘルパー関数で定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  helper_method :current_cart
+
   def current_cart
     if user_signed_in?
       current_user.cart || current_user.create_cart!
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,4 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: :show
-
   def show
-  end
-
-  private
-
-  def set_cart
-    @cart = current_cart
   end
 end
