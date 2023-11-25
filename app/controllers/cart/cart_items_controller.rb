9cecfb72cc3e18023b583fb830ad4cf9d9c61294
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-25 22:43:53 +0900

    レビュー対応: アップデートの処理から、cart_itemの取得処理は切り離す

--- app/controllers/cart/cart_items_controller.rb
+++ app/controllers/cart/cart_items_controller.rb
@@ -1,4 +1,6 @@
 class Cart::CartItemsController < ApplicationController
+  before_action :set_cart_item, only: %i[update]
+
   def create
     product = Product.purchasable.find(cart_item_params[:product_id])
     current_cart.add_cart_item(product, cart_item_params[:quantity])
@@ -6,11 +8,17 @@ class Cart::CartItemsController < ApplicationController
   end
 
   def update
-    current_cart.cart_items.find(params[:id]).update_quantity!(cart_item_params[:quantity])
+    @cart_item.update_quantity!(cart_item_params[:quantity])
     redirect_to cart_url, notice: '商品個数を更新しました'
   end
 
   def cart_item_params
     params.require(:cart_item).permit(:product_id, :quantity)
+  end
+
+  private
+
+  def set_cart_item
+    @cart_item = current_cart.cart_items.find(params[:id])
   end
 end
