e9a56e997fc2de22eb7f1bd0ff826f2d71cb1853
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-29 15:49:35 +0900

    レビュー対応: cart_itemのdestroyアクションは削除

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   resources :products, only: %i[index show]
   resource :cart, only: :show do
-    resources :cart_items, module: :cart, only: %i[create update destroy]
+    resources :cart_items, module: :cart, only: %i[create update]
   end
 end
