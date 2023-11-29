eee0e3082d79d7bd6c275130b54d8ed93bdcff39
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-29 17:01:23 +0900

    fix: 商品ページから追加すると常に1つになってしまうバグ修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,5 +7,11 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.purchasable.find(params[:id])
+    if user_signed_in? && current_cart.cart_items
+      cart_item = current_cart.cart_items.find_by(product_id: @product.id)
+      @cart_item_quantity = cart_item ? cart_item.quantity : 0
+    else
+      @cart_item_quantity = 0
+    end
   end
 end
