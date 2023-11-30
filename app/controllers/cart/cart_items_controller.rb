fa4c52980bd87f28e9e7a5dde34ed5aaded550a1
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-11-30 20:16:53 +0900

    レビュー対応: ユーザー入力数値の制限をコントローラーに追加

--- app/controllers/cart/cart_items_controller.rb
+++ app/controllers/cart/cart_items_controller.rb
@@ -1,15 +1,18 @@
 class Cart::CartItemsController < ApplicationController
   before_action :set_cart_item, only: %i[update]
 
+  MIN_QUANTITY = 1
+
   def create
+    quantity = [cart_item_params[:quantity].to_i, MIN_QUANTITY].max
     product = Product.purchasable.find(cart_item_params[:product_id])
-    current_cart.add_cart_item(product, cart_item_params[:quantity])
+    current_cart.add_cart_item(product, quantity)
     redirect_to cart_url, notice: '商品をカートに追加しました'
   end
 
   def update
-    quantity = cart_item_params[:quantity].to_i
-    if quantity == 0  # rubocop:disable all
+    quantity = [cart_item_params[:quantity].to_i, 0].max
+    if quantity == 0
       @cart_item.destroy!
       redirect_to cart_url, notice: '商品をカートから削除しました'
     else
